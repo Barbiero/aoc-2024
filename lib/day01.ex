5a6e725339e34beb6ce517a3992eb49268722093
Pedro Barbiero <pedrobarbiero@gmail.com>
2024-12-02 19:52:50 +0000

    day01 part 1

---
+++ lib/day01.ex
@@ -0,0 +1,27 @@
+defmodule Day01 do
+  def calcListDistances(list01, list02) when is_list(list01) and is_list(list02) do
+    list01 = list01 |> Enum.sort()
+    list02 = list02 |> Enum.sort()
+
+    [list01, list02] |> List.zip() |> Enum.map(fn {a, b} -> abs(b - a) end) |> Enum.sum()
+  end
+
+  def readLineNumbers(line) when is_binary(line) do
+    line
+    |> String.trim()
+    |> String.split(~r"\s+")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  def readInputFile(filename) do
+    File.stream!(filename)
+    |> Enum.map(&readLineNumbers/1)
+    |> Enum.reduce({[], []}, fn {first, second}, {list01, list02} ->
+      {[first | list01], [second | list02]}
+    end)
+    |> Tuple.to_list()
+    |> Enum.map(&Enum.reverse/1)
+    |> List.to_tuple()
+  end
+end
---
+++ test/day01_test.exs
@@ -0,0 +1,28 @@
+defmodule Day01Test do
+  use ExUnit.Case
+  doctest Day01
+
+  test "compares lists and returns the sum of the distances" do
+    list01 = [3, 4, 2, 1, 3, 3]
+    list02 = [4, 3, 5, 3, 9, 3]
+
+    assert Day01.calcListDistances(list01, list02) == 11
+  end
+
+  test "reads a line correctly" do
+    assert Day01.readLineNumbers("123 346\n") == {123, 346}
+  end
+
+  test "reads example file correctly" do
+    {list01, list02} = Day01.readInputFile("./inputs/day01_example.txt")
+
+    assert list01 == [3, 4, 2, 1, 3, 3]
+    assert list02 == [4, 3, 5, 3, 9, 3]
+  end
+
+  test "calculate result" do
+    {list01, list02} = Day01.readInputFile("./inputs/day01.txt")
+
+    assert Day01.calcListDistances(list01, list02) == 2_000_468
+  end
+end
